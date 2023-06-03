bda79ceb3e3d3b727d02cba1a052a1e4f1b40f4a
luciosantibañez <luciosantibanez99@gmail.com>
2023-06-03 20:22:13 -0300

    EntregaFinalSantibañez

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,4 @@
-
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import ItemList from '../ItemList/ItemList';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
