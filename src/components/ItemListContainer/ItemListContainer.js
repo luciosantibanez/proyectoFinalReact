34210db32a010c056f1ff466566485bccf91f122
luciosantibañez <luciosantibanez99@gmail.com>
2023-06-03 20:21:30 -0300

    intentando hacer funcionar mi base de datos de firebase

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,14 +1,36 @@
-// ItemListContainer.js
+
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ItemList from '../ItemList/ItemList';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig from '../../firebase-config';
+
+// Inicializa la conexión a Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 const ItemListContainer = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const querySnapshot = await db.collection('items').get();
+        const itemsData = querySnapshot.docs.map((doc) => doc.data());
+        setItems(itemsData);
+      } catch (error) {
+        console.log('Error fetching items:', error);
+      }
+    };
+
+    fetchItems();
+  }, []);
   
   return (
     <div>
-      <h2>Product List      </h2>
-      <ItemList/>
+      <h2>Lista de productos</h2>
+      <ItemList items={items} />
     </div>
   );
 };
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,8 @@
-export default{
-    
+import {initializeApp} from 'firebase/app'
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
     apiKey: "AIzaSyBi00MfqUoRqZlH4GMxQ0HF4nTSLfBrhLQ",
     authDomain: "ergo-f4f95.firebaseapp.com",
     projectId: "ergo-f4f95",
@@ -7,4 +10,7 @@ export default{
     messagingSenderId: "1030236219518",
     appId: "1:1030236219518:web:281da02d9fac6ea528d02f",
     measurementId: "G-F9Q4KLMWMP"
-}
+};
+
+initializeApp(firebaseConfig);
+export default firebase;
